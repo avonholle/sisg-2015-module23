43f932389715dc09da4a06837a483dcde004a050
Ann Von Holle <vonholle@email.unc.edu>
2015-07-22 16:12:47 -0700

    Practical 1 files -- first day

---
+++ practical_1/sisg_aqg_2015_p1.R
@@ -0,0 +1,26 @@
+# ===============================================================
+# Summer Institute in Statistical Genetics, 2015, Seattle
+# Advanced Quantitative Genetics
+# Practical 1 
+# Date:
+# Author:
+# ===============================================================
+#setwd("~/Desktop/SISG_AQG_2015") # Mac and Linux
+
+setwd("C:/Users/vonholle/Documents/grad_school/misc/training/sisg-2015/module23/sisg-2015-module23") # Windows
+
+args(rnorm)
+height <- rnorm(1000, 170, 2.8)
+mean(height)
+sd(height)
+plot(density(height, adjust = 3), main = '')
+
+y.means <- c(5, 15, 20)
+y.sd <- 5
+
+# Generate a SNP. Sample from the binomial distribution with success probability 0.4
+# Success probability corresponds to the MAF
+snp <- rbinom(1000, 2, 0.4)
+# Generate Y given the SNP and bind to data frame
+y <- rnorm(1000, y.means[factor(snp)], y.sd)
+snp.data <- data.frame(cbind(y, snp))
